4334320fa310650061f34f2e67b61ae07891ee01
knromaric <nzekeng@gmail.com>
2019-01-07 13:36:25 +0200

    first winforms application

---
+++ FirstWinForms/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace FirstWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.txtBoxAge = new System.Windows.Forms.TextBox();
+            this.btnDisplayData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 43);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(12, 134);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 1;
+            this.lblAge.Text = "Age";
+            
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(12, 74);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(381, 20);
+            this.txtBoxName.TabIndex = 2;
+            // 
+            // txtBoxAge
+            // 
+            this.txtBoxAge.Location = new System.Drawing.Point(15, 169);
+            this.txtBoxAge.Name = "txtBoxAge";
+            this.txtBoxAge.Size = new System.Drawing.Size(378, 20);
+            this.txtBoxAge.TabIndex = 3;
+            // 
+            // btnDisplayData
+            // 
+            this.btnDisplayData.Location = new System.Drawing.Point(317, 262);
+            this.btnDisplayData.Name = "btnDisplayData";
+            this.btnDisplayData.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplayData.TabIndex = 4;
+            this.btnDisplayData.Text = "Display Data";
+            this.btnDisplayData.UseVisualStyleBackColor = true;
+            this.btnDisplayData.Click += new System.EventHandler(this.btnDisplayData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 310);
+            this.Controls.Add(this.btnDisplayData);
+            this.Controls.Add(this.txtBoxAge);
+            this.Controls.Add(this.txtBoxName);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.TextBox txtBoxAge;
+        private System.Windows.Forms.Button btnDisplayData;
+    }
+}
+
---
+++ FirstWinForms/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FirstWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplayData_Click(object sender, EventArgs e)
+        {
+            string name = txtBoxName.Text;
+            string age = txtBoxAge.Text;
+            string message = $"{name} is {age}";
+            MessageBox.Show(message);
+        }
+    }
+}
