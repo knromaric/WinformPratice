30586ff5885f3e29c51c0475d715575944045307
knromaric <nzekeng@gmail.com>
2019-01-07 17:17:08 +0200

    Email Validation

---
+++ EmailValidation/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace EmailValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnAddEmail = new System.Windows.Forms.Button();
+            this.txtEmailList = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(16, 71);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(226, 20);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // btnAddEmail
+            // 
+            this.btnAddEmail.Location = new System.Drawing.Point(279, 67);
+            this.btnAddEmail.Name = "btnAddEmail";
+            this.btnAddEmail.Size = new System.Drawing.Size(75, 23);
+            this.btnAddEmail.TabIndex = 2;
+            this.btnAddEmail.Text = "Add";
+            this.btnAddEmail.UseVisualStyleBackColor = true;
+            this.btnAddEmail.Click += new System.EventHandler(this.btnAddEmail_Click);
+            // 
+            // txtEmailList
+            // 
+            this.txtEmailList.Location = new System.Drawing.Point(16, 109);
+            this.txtEmailList.Name = "txtEmailList";
+            this.txtEmailList.Size = new System.Drawing.Size(226, 96);
+            this.txtEmailList.TabIndex = 3;
+            this.txtEmailList.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 247);
+            this.Controls.Add(this.txtEmailList);
+            this.Controls.Add(this.btnAddEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnAddEmail;
+        private System.Windows.Forms.RichTextBox txtEmailList;
+    }
+}
+
---
+++ EmailValidation/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailValidation
+{
+    public partial class Form1 : Form
+    {
+        private readonly string emailRegex;
+        StringBuilder emailAddress = new StringBuilder();
+
+        public Form1()
+        {
+            
+            emailRegex = @"[a-z0-9!#$%&'*+/=?^_`{|}~-]" +
+
+                        @"+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@" +
+
+                        @"(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)" +
+
+                        @"+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddEmail_Click(object sender, EventArgs e)
+        {
+            var result = Regex.IsMatch(txtEmail.Text, emailRegex);
+
+            if (result)
+            {
+                txtEmail.BackColor = Color.White;
+                if (!emailAddress.ToString().Contains(txtEmail.Text))
+                    emailAddress.Append(txtEmail.Text + "\n");
+
+                DisplayEmails();
+            }
+            else
+            {
+                txtEmail.BackColor = Color.Pink;
+                txtEmailList.Text = emailAddress.ToString();
+                DisplayEmails();
+            }
+        }
+
+        private void DisplayEmails()
+        {
+            txtEmailList.Text = emailAddress.ToString();
+        }
+    }
+}
