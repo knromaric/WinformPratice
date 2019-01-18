8d5548f294534e782a0fa4dfdd7c2fc3f9b113d9
knromaric <nzekeng@gmail.com>
2019-01-18 09:48:12 +0200

    Added Calculator.Tests

---
+++ Calculator.Test.Unit/InputConverterTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Test.Unit
+{
+    
+    [TestClass]
+    public class InputConverterTest
+    {
+        private readonly InputConverter _inputConverter = new InputConverter();
+
+        [TestMethod]
+        public void ConvertValidStringInputIntoDouble()
+        {
+            string inputNumber = "5";
+
+            double result = _inputConverter.ConvertInputToNumeric(inputNumber);
+
+            Assert.AreEqual(5, result);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FailsToConvertInvalidInputIntoDouble()
+        {
+            string inputNumber = "djd";
+            double result = _inputConverter.ConvertInputToNumeric(inputNumber);
+        }
+    }
+}
---
+++ Calculator.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d2ce797-65b3-439d-a3ac-53b9636da633")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
