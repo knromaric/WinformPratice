4f9aebc130d2e971edc52eca0ab7a6595278a030
knromaric <nzekeng@gmail.com>
2019-01-21 17:15:58 +0200

    Add Sudoku Project

---
+++ SudokuSolver/Workers/SudokuBoardDisplayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Workers
+{
+    class SudokuBoardDisplayer
+    {
+        public void Display(string title, int[,] sudokuBoard)
+        {
+            if (!title.Equals(string.Empty))
+            {
+                Console.WriteLine($"{title} {Environment.NewLine}");
+            }
+
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                Console.Write("|");
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    Console.WriteLine($"{sudokuBoard[row, col]} |");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+         }
+    }
+}
---
+++ SudokuSolver/Workers/SudokuFileReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SudokuSolver.Workers
+{
+    class SudokuFileReader
+    {
+        public int[,] ReadFile(string fileName)
+        {
+            int[,] sudokuBoard = new int[9, 9];
+            try
+            {
+                var sudokuBoardLines = File.ReadAllLines(fileName);
+
+                int row = 0;
+
+                foreach (var sudokuBoardLine in sudokuBoardLines)
+                {
+                    var sudokuLineElements = sudokuBoardLine.Split('|').Skip(1).Take(9).ToArray();
+                    int col = 0;
+
+                    foreach(var sudokuLineElement in sudokuLineElements)
+                    {
+                        sudokuBoard[row, col] =sudokuLineElement.Equals(" ") ? 0 : Convert.ToInt16(sudokuLineElement);
+                        col++;
+                    }
+
+                    row++;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Something went wrong while reading the file: " + ex.Message);
+            }
+
+            return sudokuBoard; 
+        }
+    }
+}
